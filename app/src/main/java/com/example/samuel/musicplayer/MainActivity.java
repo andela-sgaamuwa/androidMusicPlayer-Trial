3c7c49b04c7d32cf0901ae0137c40199836a478c
Samuel Gaamuwa <samuel.gaamuwa@spatialkey.com>
2017-09-05 02:11:32 +0300

    initial commit for this

---
+++ app/src/main/java/com/example/samuel/musicplayer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.samuel.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.hotel);
+
+        Button playButton = (Button) findViewById(R.id.play);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
