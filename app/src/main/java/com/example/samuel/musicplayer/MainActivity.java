bfb8d02e904efb36ca9966156f0db7e3ccb46b5a
Samuel Gaamuwa <samuel.gaamuwa@spatialkey.com>
2017-09-08 05:03:27 +0300

    Dealing with onCompletionListeners

--- app/src/main/java/com/example/samuel/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/samuel/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.hotel);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.phrase_how_are_you_feeling);
 
         Button playButton = (Button) findViewById(R.id.play);
 
@@ -30,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mediaPlayer.pause();
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(getApplicationContext(), "It's done", Toast.LENGTH_SHORT).show();
             }
         });
     }
